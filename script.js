7bf566cd4ccb5b225aef65f2ee8730b074b6377e
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2022-11-09 21:33:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const msgEl = document.getElementById("msg");
+const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
 
-console.log("Number: ", randomNum);
+console.log('Number:', randomNum);
 
-window.speechRecognition =
-  window.speechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let recognition = new window.speechRecognition();
+let recognition = new window.SpeechRecognition();
 
-//start recognition and game
+// Start recognition and game
 recognition.start();
 
-//capture user speak
+// Capture user speak
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
@@ -20,75 +20,57 @@ function onSpeak(e) {
   checkNumber(msg);
 }
 
-//check message against number
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
 function checkNumber(msg) {
   const num = +msg;
 
-  //check if valid number
+  // Check if valid number
   if (Number.isNaN(num)) {
-    msgEl.innerHTML += `
-		<div>
-		Not a valid number 
-		</div>
-`;
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
     return;
   }
 
-  //check in range
+  // Check in range
   if (num > 100 || num < 1) {
-    msgEl.innerHTML += `
-		<div>
-		Number must be between 1 and 100
-		</div>
-		`;
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
     return;
   }
 
-  //check number
+  // Check number
   if (num === randomNum) {
     document.body.innerHTML = `
-		<h2>Congratulations! You have guessed the number <br> <br>
-		It was ${num}</h2>
-		<button class="play-again" id="play-again">
-		Play Again
-</button>
-		`;
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
   } else if (num > randomNum) {
-    msg.innerHTML += `
-		<div>
-		GO LOWER
-     </div>
-     `;
+    msgEl.innerHTML += '<div>GO LOWER</div>';
   } else {
-    msgEl.innerHTML += `
-	  <div>
-	  GO HIGHER 
-	  </div>
-	  `;
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
   }
 }
 
-//write what user speaks
-function writeMessage(msg) {
-  msgEl.innerHTML = `
-	<div>You said: </div>
-	<span class="box">${msg}</span>
-	`;
-}
-
-//generate random number
+// Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-//speak result
-recognition.addEventListener("result", onSpeak);
+// Speak result
+recognition.addEventListener('result', onSpeak);
 
-//end speech recognition service
-recognition.addEventListener("end", () => recognition.start());
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
 
-document.body.addEventListener("click", (e) => {
-  if (e.target.id === `play-again`) {
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'play-again') {
     window.location.reload();
   }
 });
